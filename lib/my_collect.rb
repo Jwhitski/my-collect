4415801e58e6d0cdac0deafaab286d90e2506681
Jason Whitlock <jrwhitlock7@gmail.com>
2021-02-11 16:48:32 -0700

    commit tree 9e5f9ee2361a349701d3661894982b9823e3e7da

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
+def my_collect(arr)
+    i = 0
+    new_arr = []
+    while i < arr.length
+        # binding.pry
+        new_arr << yield(arr[i])
+        i += 1
+    end
+    new_arr
+end
 
-
+#  my_collect(new_arr) do |lang| 
+#     lang.upcase
+#  end
